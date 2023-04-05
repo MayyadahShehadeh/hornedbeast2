f3e4a517722e51e843e7d1e3f5b8d26bc3e6be4f
MayyadahShehadeh <miss.mayadah1@gmail.com>
2023-04-05 17:37:14 +0000

    lab2

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,18 +1,40 @@
 import React from "react";
 
-class HornedBeast extends React.Component{
-render(){
-    return(
 
 
-        <>
+class HornedBeast extends React.Component {
 
-<img src={this.props.img}/>
-<h2> {this.props.title}</h2>
-<p>{this.props.description}</p>
-        </>
-    )
-}
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numOfClick: 0
+        }
+    }
+    increaseOne = () => {
+        this.setState({
+            numOfClick: this.state.numOfClick + 1
+
+        })
+    }
+
+
+
+
+
+    render() {
+        return (
+
+
+            <>
+
+                <img src={this.props.img} onClick={this.increaseOne} />
+                <h2> {this.props.title}</h2>
+                <p>{this.props.description}</p>
+                <p> ❤️{this.state.numOfClick}</p>
+            </>
+        )
+    }
 }
 
 export default HornedBeast;
