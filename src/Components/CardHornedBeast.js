a6469ff1e8d833925d55594385b895c0328ef4d4
MayyadahShehadeh <miss.mayadah1@gmail.com>
2023-04-07 23:35:30 +0000

    lab4

---
+++ src/Components/CardHornedBeast.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Card,Col,Button } from "react-bootstrap";
+
+class CardHornedBeast extends React.Component {
+
+    render() {
+
+        return (
+
+            <>
+                <Col>
+                    <Card style={{ width: '15rem', margin: '5px' }}>
+                        <Card.Img variant="top" src={this.props.img} />
+                        <Card.Body>
+                            <Card.Title>{this.props.title}</Card.Title>
+                            <Card.Text>
+                                ❤️{this.props.numOfClick} Favorite
+                            </Card.Text>
+                            <p> {this.props.horns} horns </p>
+                            <Card.Text>
+                                {this.props.description}
+                            </Card.Text>
+                            <Button variant="primary" onClick={this.props.increaseOne}> vote</Button>
+
+                            <Button variant="primary" onClick={this.props.handleShow} style={{ margin: '10px', backgroundColor: 'pink', borderColor: 'pink', color: 'black' }}>
+                                show
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+            </>
+        )
+    }
+
+}
+
+export default CardHornedBeast;
---
+++ src/Components/ModalHornedBeast.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Modal,Card,Button } from "react-bootstrap";
+
+class ModalHornedBeast extends React.Component {
+
+    render() {
+
+        return (
+            <>
+
+                <Modal show={this.props.handleShow} onHide={this.props.handleClose} >
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Card.Img variant="top" src={this.props.img} />
+                    <Modal.Body>{this.props.description}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.props.handleClose}> Close</Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </>
+
+
+        )
+    }
+}
+
+export default ModalHornedBeast;
